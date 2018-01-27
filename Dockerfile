2b28f7c24b867bba0b4a3619022dfcf64a8b5ac2
Yusaku Maezono <yusakumaezono@gmail.com>
2018-01-27 15:04:41 +0900

    add npm install mysql, jayson, getops, log4js

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM node:latest
 
 RUN npm install mysql
+RUN npm install jayson
+RUN npm install getopts
+RUN npm install log4js
--- app.js
+++ app.js
@@ -1,16 +1,57 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
+const log4js = require('log4js');
+const getopts = require('getopts')
+const jayson = require('jayson');
+const fs = require('fs');
 
-var connection = mysql.createConnection({
+// args
+const options = getopts(process.argv.slice(2), {
+  default: {
+    network: 'ropsten',
+    single: false
+  }
+})
+
+// files
+const lockFile = '/var/tmp/explorer-' + options.network + '.lock'
+const lastFile = '/var/tmp/explorer-' + options.network + '.last-block'
+const errorLog = '/var/tmp/explorer-' + options.network + '.errors'
+
+// logs
+log4js.configure({
+  appenders: { explorer: { type: 'file', filename: errorLog } },
+  categories: { default: { appenders: ['explorer'], level: 'debug' } }
+});
+const logger = log4js.getLogger('explorer');
+
+// database
+const connection = mysql.createConnection({
   host     : 'database',
   user     : 'admin',
-  password : '12dlql*41',
+  password : '12dlql*41X',
   database : 'explorer'
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
-});
+// json rpc
+const url = options.runType === 'mainnet' ? 'https://api.myetherapi.com/eth' : 'https://api.myetherapi.com/rop';
+const client = jayson.client.https(url);
+
+// create lock file
+if (fs.existsSync(lockFile)) {
+  logger.error('detected lockfile at ' + lockFile + ' ... exiting');
+  return;
+} else {
+  fs.writeFile(lockFile, null, { flag: 'wx' }, function (err) {
+    if (err) {
+      logger.error('error while trying to create lock file');
+      return;
+    }
+  });
+}
+
+// current block
+if(options.block){
+  fs.readFile(lastFile, 'utf8', function (err, text) {
+    console.log(text);
+  });
+}
